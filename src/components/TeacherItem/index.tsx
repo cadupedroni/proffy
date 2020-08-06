ca39e2faf7974b158ea1b504d92906d9ab8efa42
Carlos Vieira <carlos.vieira@outlook.com>
2020-08-06 16:36:10 -0300

    Integration  - Teacher List

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,25 +4,36 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+  id: number,
+  name: string,
+  avatar: string,
+  whatsapp: string,
+  bio: string,
+  subject: string,
+  cost: number,
+};
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/2206405?s=460&u=82eccdf54c13b05d29a2c761b3ce54356b288634&v=4" alt="Cadu Pedroni" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Cadu Pedroni</strong>
-          <span>Tecnologia</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
       <p>
-        Entusiasta das melhores tecnologias avançadas.
-        <br /><br />
-        Apaixonado por ensinar e mudar a vida das pessoas profissionalmente.
+        {teacher.bio}
       </p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>{teacher.cost}</strong>
         </p>
         <button type="button">
           <img src={whatsappIcon} alt="Whatsapp"/>
